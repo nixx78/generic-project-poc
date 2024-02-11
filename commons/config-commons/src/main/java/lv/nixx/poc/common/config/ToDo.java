4da4a44830b49007e9cc72686f212695819c3702
nixx78 <nixx78@gmail.com>
2024-02-11 11:45:04 +0200

    Projects refactoring and cleanup

--- commons/config-commons/src/main/java/lv/nixx/poc/common/config/ToDo.java
+++ commons/config-commons/src/main/java/lv/nixx/poc/common/config/ToDo.java
@@ -3,14 +3,15 @@ package lv.nixx.poc.common.config;
 public class ToDo {
 
     // Project structure tasks
-    //TODO Create project with common dependencies libs-commons
-    //TODO commons-proj -> config-commons ?
 
     // DB connection related tasks
     //TODO DB connection URL -> hardcode ?
 
     // Security configuration tasks
     //TODO @LoggedUserController -> добавить настройку для контроля списка полей, который будет возвращать /service/login, настройка URL
+
+    //Common
+    //TODO Investigate possibility create beans on 'fly' = https://www.woolha.com/tutorials/spring-register-bean-programmatically-examples
 
 
 }
